29b344178b614121de5a355b3f143ff2f4a45199
yxx <793505879@qq.com>
2021-09-29 17:34:30 +0800

    init project

---
+++ plugins/web3modal-vue.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-27 13:51:17
+ * @LastEditTime: 2021-09-27 17:26:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Indutri\plugins\web3modal-vue.js
+ */
+import Vue from 'vue'
+import Web3ModalVue from "web3modal-vue";
+
+Vue.component('Web3ModalVue', Web3ModalVue)
